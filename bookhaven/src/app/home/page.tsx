96d09c058afa1d17439f2a8af5ec2b35afe2183d
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-04-23 22:40:51 +0500

    Home Working..

--- bookhaven/src/app/home/page.tsx
+++ bookhaven/src/app/home/page.tsx
@@ -1,8 +1,45 @@
+import AnimatedText from '@/components/ui/AnimatedText'
+import BookCard from '@/components/ui/BookCard'
+import Hero from '@/components/ui/Hero'
+import { books } from '@/lib/constants'
 import React from 'react'
+
+
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+    <main>
+      <Hero/>
+
+      <section className="py-12 md:py-24 bg-muted/20">
+      <div className="container space-y-12">
+        <div className="text-center space-y-4">
+          <AnimatedText
+          text='Featured Collection'
+          className='text-3xl md:text-4xl font-bold'/>  
+          <p className="text-lg text-muted-foreground max-w-2xl mx-auto">
+            Discover our handpicked selection of books for every reader
+          </p>
+          </div>
+
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {books.map((book, index) => (
+              <BookCard
+              key={index}
+              title={book.title}
+              author={book.author}
+              price={book.price}
+              rating={book.rating}
+              coverImage={book.coverImage}/>
+            ))}
+          </div>
+      </div>
+      </section>
+
+      
+    </main>
+    </>
   )
 }
 
