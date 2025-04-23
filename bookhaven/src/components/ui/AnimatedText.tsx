62d8284d5d920767ea97470ffb33628f151265b1
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-04-23 21:40:15 +0500

    Working..

--- bookhaven/src/components/ui/AnimatedText.tsx
+++ bookhaven/src/components/ui/AnimatedText.tsx
@@ -1,6 +1,36 @@
-import React from 'react'
+"use client"
 
-const AnimatedText = () => {
+import React from 'react'
+import { motion } from 'framer-motion';
+
+
+const AnimatedText = ({text, className,}: {text:string, className?: string}) => {
+  const words = text.split("  ")
+
+  const container = {
+    hidden: {opacity: 0},
+    visible: ( i = 1) => ({
+      opacity: 1,
+      transition: { staggerChildren: 0.12, delayChildren: 0.04 * i },
+    }),
+  }
+
+  const child = {
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 100,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      
+    }
+  }
+
   return (
     <div>AnimatedText</div>
   )
