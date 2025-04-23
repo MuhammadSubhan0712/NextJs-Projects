3cddbe61ac42f8556c5ea2b95a5e518579efda51
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-04-23 19:25:48 +0500

    Working..

--- bookhaven/src/components/ui/Navbar.tsx
+++ bookhaven/src/components/ui/Navbar.tsx
@@ -1,8 +1,19 @@
+"use client"
 import React from 'react'
 
 const Navbar = () => {
+
+    const navItems = [
+        {name : "Home" , href: "/"},
+        {name : "About" , href: "/about"},
+        {name : "Services" , href: "/services"},
+        {name : "Contact" , href: "/contact"},
+    ];
+
   return (
-    <div>Navbar</div>
+    <>
+    
+    </>
   )
 }
 
