9fb7eb21c255c2735de1e7064efb2e6572b28118
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-04-24 20:00:31 +0500

    Working..

--- bookhaven/src/components/ui/BookCard.tsx
+++ bookhaven/src/components/ui/BookCard.tsx
@@ -1,8 +1,9 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { Star, ShoppingCart } from "lucide-react";
+import { div } from "framer-motion/client";
 
 interface BookCardProps {
   title: string;
@@ -19,6 +20,47 @@ const BookCard = ({
   rating,
   coverImage,
 }: BookCardProps) => {
+
+  const [isClient , setIsClient] = useState(false);
+
+  useEffect(()=>{
+    setIsClient(true);
+  },[]);
+
+  if (!isClient) {
+    return (
+      <div className="border rounded-lg overflow-hidden shadow-sm hovre:shadow-md transition-shadow">
+        <div className="absolute inset-0 flex items-center justify-center text-5xl">
+            {coverImage}
+          </div>
+          
+          <div className="p-4 space-y-2">
+          <h3 className="font-medium">{title}</h3>
+          <p className="text-sm text-muted-foreground">{author}</p>
+          <div className="flex items-center gap-1">
+            {[...Array(5)].map((_, i) => (
+              <Star
+                key={i}
+                className={`h-4 w-4 ${
+                  i < rating
+                    ? "fill-primary text-primary"
+                    : "text-muted-foreground"
+                }`}
+              />
+            ))}
+          </div>
+
+          <div className="flex items-center justify-between pt-2">
+            <span className="font-bold">${price.toFixed(2)}</span>
+            <Button variant="outline" size="sm">
+              <ShoppingCart className="h-4 w-4 mr-2" />
+              Add
+            </Button>
+          </div>
+        </div>
+      </div>
+    )
+  }
   return (
     <>
       <motion.div
