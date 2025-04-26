372c1270956acc0bcf62f4786cb35c1d35b501b7
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-04-26 17:29:53 +0500

    Working./.

--- icecream-bar/src/components/IceCreamAnimation.tsx
+++ icecream-bar/src/components/IceCreamAnimation.tsx
@@ -1,5 +1,6 @@
+"use client";
 import React from "react";
-import { delay, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const IceCreamAnimation = () => {
   const flavors = [
--- icecream-bar/src/components/PageTransition.tsx
+++ icecream-bar/src/components/PageTransition.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import { motion, AnimatePresence } from 'framer-motion';
-import { useRouter } from 'next/router';
+"use client";
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { usePathname } from "next/navigation";
 
-const PageTransition = () => {
+const PageTransition = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+
   return (
     <>
-
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={pathname}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.5 }}>
+          {children}
+        </motion.div>
+      </AnimatePresence>
     </>
-  )
-}
+  );
+};
 
-export default PageTransition
+export default PageTransition;
