4eb186f781b95f39fe7c3cb277b7016414c1ad18
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-04-26 17:34:56 +0500

    Working..

--- icecream-bar/src/components/Footer.tsx
+++ icecream-bar/src/components/Footer.tsx
@@ -1,4 +1,25 @@
 import React from 'react'
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { Facebook, Instagram, Twitter } from 'lucide-react';
+
+
+const footerLinks = [
+  {name: 'About Us' , href: "/about"},
+  {name: 'Our Flavors' , href: "/services"},
+  {name: 'Locations' , href: "/contact"},
+  {name: 'Careers' , href: "#"},
+  {name: 'Privacy Policy' , href: "#"},
+  {name: 'Terms of Service' , href: "#"},
+];
+
+const socialLinks = [
+  {icon: <Facebook className='w-5 h-5'/>, href: "#"},
+  {icon: <Instagram className='w-5 h-5'/>, href: "#"},
+  {icon: <Twitter className='w-5 h-5'/>, href: "#"},
+
+]
+
 
 const Footer = () => {
   return (
