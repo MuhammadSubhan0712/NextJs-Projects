dcc863b4456fb0c0ae21dcd2af563409174dac9a
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-04-28 01:27:13 +0500

    Working.

--- icecream-bar/src/components/IcecreamModel.tsx
+++ icecream-bar/src/components/IcecreamModel.tsx
@@ -1,46 +1,55 @@
-"use client"
+"use client";
 
-import React, { useRef } from 'react'
+import React, { useRef } from "react";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { useAnimation } from 'framer-motion';
+import { useAnimation } from "framer-motion";
 
+const IcecreamModel = ({
+  position = [0, 0, 0],
+  scale = 1,
+  flavor = "vanilla",
+}) => {
+  const group = useRef();
+  const { nodes, materials, animations } = useGLTF("/models/icecream.glb");
+  const { actions } = useAnimation(animations, group);
 
-
-const IcecreamModel = ({ position = [0, 0, 0], scale = 1, flavor = "vanilla" }) => {
-   
-    const group = useRef();
-    const { nodes, materials, animations } = useGLTF('/models/icecream.glb');
-    const { actions } = useAnimation(animations, group);
-
-
-    // Animation Loop:
-    useFrame((state) => {
-        if (group.current) {
-            group.current.rotation.y += 0.005;
-            group.current.position.y = Math.sin(state.clock.getElapsedTime() * 0.2);
-        }
-    });
-
-    // Set material based on flavor:
-    let materialColor;
-    switch(flavor){
-    case 'chocolate':
-        materialColor = '#5c4033';
-        break;
-    case 'strawberry':
-        materialColor = '#ff6b8b';
-        break;
-    default: //Vanilla
-        materialColor = '#f3e5ab';
-        break;
+  // Animation Loop:
+  useFrame((state) => {
+    if (group.current) {
+      group.current.rotation.y += 0.005;
+      group.current.position.y = Math.sin(state.clock.getElapsedTime() * 0.2);
     }
+  });
+
+  // Set material based on flavor:
+  let materialColor;
+
+  switch (flavor) {
+    case "chocolate":
+      materialColor = "#5c4033";
+      break;
+    case "strawberry":
+      materialColor = "#ff6b8b";
+      break;
+    default: //Vanilla
+      materialColor = "#f3e5ab";
+      break;
+  }
 
   return (
     <>
-    
-    </>
-  )
-}
+      <group ref={group} position={position} scale={scale} dispose={null}>
+        <mesh
+          geometry={nodes.icecream.geometry}
+          material={nodes.icecream.matreial}
+          material-color={materialColor}
+        />
 
-export default IcecreamModel
+        <mesh geometry={nodes.cone.geometry} material={nodes.cone.material} />
+      </group>
+    </>
+  );
+};
+
+export default IcecreamModel;
