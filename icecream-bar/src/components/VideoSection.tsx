ab2a70fbfb1f29fc89f3c1b8fd7efd6004bb0708
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-04-28 01:55:06 +0500

    Working.

--- icecream-bar/src/components/VideoSection.tsx
+++ icecream-bar/src/components/VideoSection.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+"use client"
+
+import { motion, useAnimation, useInView } from 'framer-motion';
+import React, { useEffect, useRef } from 'react'
 
 const VideoSection = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, {once: false, amount:0.5});
+  const controls = useAnimation();
+  
+  useEffect(() => {
+    if (isInView) {
+      controls.start("visible");
+    }
+    else{
+      controls.start("hidden");
+    }
+  },[isInView, controls]);
+
   return (
-    <div>VideoSection</div>
+    <>
+    <section
+    ref={ref}
+    className='relative h-screen w-full overflow-hidden'>
+
+      {/* Video Background: */}
+      <div className="absolute inset-0 z-0">
+        <video 
+        autoPlay
+        loop
+        muted
+        playsInline
+        className='w-full h-full object-cover'>
+          <source src='/videos/icecream-parallax.mp4' type='video/mp4'/>
+        </video>
+      </div>
+
+      
+
+    </section>
+    </>
   )
 }
 
